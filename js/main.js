714bd4692898de3034afaaf1eaafbb11bbd70e62
Hanna Preston <hkpreston92@bellsouth.net>
2018-07-14 23:05:19 -0400

    final commit

--- js/main.js
+++ js/main.js
@@ -1,33 +1,49 @@
+// $(document).ready(function(){
+//     var largestHeader = $('h1');
+//     largestHeader.css({
+//         'font-size': '100px',
+//         'color': 'green',
+//         'margin-bottom': '145px'
+//     });
+//
+//     $('h2').addClass('second-header');
+//
+//     // $('h1').append('This is appended');
+//
+//     // $('h1').prepend('This is prepended ');
+//
+//     $('h1').before('This is before.');
+//
+//     $('h1').after('This is after.');
+//
+// });
+//
+// $(function() {
+//     $('p').removeClass('new-class');
+//
+//     // $('h1').on('click', function(e){
+//     //     // $(this).removeClass('second-header');
+//     //     $(this).css({
+//     //         'background-color': 'red'
+//     //     });
+//
+//     $('h1').on('click', function(e){
+//         $(this).fadeOut(5000);
+//     });
+// });
+
 $(document).ready(function(){
-    var largestHeader = $('h1');
-    largestHeader.css({
-        'font-size': '100px',
-        'color': 'green',
-        'margin-bottom': '145px'
-    });
+  var listItem = $('li');
+  // listItem.css({
+  //   'padding': '50px',
+  //   'margin-bottom': '50px'
+  // });
 
-    $('h2').addClass('second-header');
-
-    // $('h1').append('This is appended');
-
-    // $('h1').prepend('This is prepended ');
-
-    $('h1').before('This is before.');
-
-    $('h1').after('This is after.');
-
-});
-
-$(function() {
-    $('p').removeClass('new-class');
-
-    // $('h1').on('click', function(e){
-    //     // $(this).removeClass('second-header');
-    //     $(this).css({
-    //         'background-color': 'red'
-    //     });
-
-    $('h1').on('click', function(e){
-        $(this).fadeOut(5000);
-    });
+  listItem.on('click', function(e) {
+    // $('li span').remove();
+    var date = new Date();
+    date.setTime(e.timeStamp);
+    var clicked = date.toDateString();
+    $(this).append('<span class="date">' + clicked + ' ' + e.type + '</span>')
+  });
 });
